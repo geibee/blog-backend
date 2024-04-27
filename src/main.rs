7b45462f755a44b487b1caa37e41fa623068e450
regmarmcem <tknktknktknktknktknk@gmail.com>
2024-04-27 13:32:22 +0900

    [add] CORS

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod controller;
 
 use axum::{
-    http::StatusCode,
+    http::{header::CONTENT_TYPE, HeaderValue, StatusCode},
     response::{IntoResponse, Json},
     routing::get,
     Extension, Router,
@@ -9,6 +9,7 @@ use axum::{
 use rusqlite::Connection;
 use serde_json::json;
 use std::sync::{Arc, Mutex};
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -20,6 +21,12 @@ async fn main() {
         .route(
             "/posts",
             get(controller::posts::get_posts).post(controller::posts::create_posts),
+        )
+        .layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:4000".parse::<HeaderValue>().unwrap())
+                .allow_methods(Any)
+                .allow_headers(vec![CONTENT_TYPE]),
         )
         .layer(Extension(Arc::new(Mutex::new(conn))));
 
